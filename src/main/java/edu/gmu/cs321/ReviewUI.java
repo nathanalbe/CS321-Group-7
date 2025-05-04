f2febdfd78658192ed2fb3b41badc476870f9c54
FlyerWasTaken <imlewis28@gmail.com>
2025-05-03 20:37:05 -0400

    Slightly altered immigrant.java
    added first_name and last_name getter methods. Added a space between the getName return string.

--- src/main/java/edu/gmu/cs321/ReviewUI.java
+++ src/main/java/edu/gmu/cs321/ReviewUI.java
@@ -31,23 +31,19 @@ package edu.gmu.cs321;/*
  */
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import static javafx.geometry.HPos.RIGHT;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
+
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class ReviewUI extends Application {
     private Stage primaryStage;
@@ -55,21 +51,38 @@ public class ReviewUI extends Application {
     @Override
     public void start(Stage primaryStage) {
 
+        AtomicInteger modCount = new AtomicInteger();
+
+
         // IMMIGRANT VARIABLES
-        String name = "Joel Ballard";
+        String firstName = "Joel";
+        String lastName = "Ballard";
         String birthdate = "03/05/1994";
         String address = "12345 Nowhere Lane";
         String email = "JoelBall.email.com";
 
         //PETITION VARIABLES
-        String petitionID = "P00112233";
-        String petitionerID = "U9876543";
         String submissionDate = "04/04/2025";
         String status = "SUBMITTED";
 
+
+//        // IMMIGRANT VARIABLES
+//        String firstName = "";
+//        String lastName = "";
+//        String birthdate = "";
+//        String address = "";
+//        String email = "";
+//
+//        //PETITION VARIABLES
+//        int petitionID = -1;
+//        int petitionerID = -1;
+//        String submissionDate = "";
+//        String status = "";
+
+
         //OBJECT CREATION (MIGHT NOT USE)
-        Immigrant immigrant = new Immigrant(name, birthdate, address, email);
-        Petition petition = new Petition(petitionID, immigrant.getUserID(), submissionDate, status);
+        Immigrant immigrant = new Immigrant(firstName, lastName, birthdate, address, email);
+        Petition petition = new Petition(immigrant.getUserID(), submissionDate, status);
 
         this.primaryStage = primaryStage;
         primaryStage.setTitle("Petition Review");
@@ -99,25 +112,30 @@ public class ReviewUI extends Application {
         grid.add(immigrantTitle, 0, 3, 2, 2);
 
 
-        Label immigrantNameTitle = new Label("Name:");
-        grid.add(immigrantNameTitle, 0, 5);
-        TextField immigrantName = new TextField(name);
-        grid.add(immigrantName, 1, 5);
+        Label immigrantFirstNameTitle = new Label("First Name:");
+        grid.add(immigrantFirstNameTitle, 0, 5);
+        TextField immigrantFirstName = new TextField(immigrant.getFirst_name());
+        grid.add(immigrantFirstName, 1, 5);
+
+        Label immigrantLastNameTitle = new Label("Last Name:");
+        grid.add(immigrantLastNameTitle, 0, 6);
+        TextField immigrantLastName = new TextField(immigrant.getLast_name());
+        grid.add(immigrantLastName, 1, 6);
 
         Label immigrantBDTitle = new Label("Birthdate:");
-        grid.add(immigrantBDTitle, 0, 6);
-        TextField immigrantBD = new TextField(birthdate);
-        grid.add(immigrantBD, 1, 6);
+        grid.add(immigrantBDTitle, 0, 7);
+        TextField immigrantBD = new TextField(immigrant.getBirthdate());
+        grid.add(immigrantBD, 1, 7);
 
         Label immigrantAddressTitle = new Label("Address:");
-        grid.add(immigrantAddressTitle, 0, 7);
-        TextField immigrantAddress = new TextField(address);
-        grid.add(immigrantAddress, 1, 7);
+        grid.add(immigrantAddressTitle, 0, 8);
+        TextField immigrantAddress = new TextField(immigrant.getAddress());
+        grid.add(immigrantAddress, 1, 8);
 
         Label immigrantEmailTitle = new Label("Email:");
-        grid.add(immigrantEmailTitle, 0, 8);
-        TextField immigrantEmail = new TextField(email);
-        grid.add(immigrantEmail, 1, 8);
+        grid.add(immigrantEmailTitle, 0, 9);
+        TextField immigrantEmail = new TextField(immigrant.getEmail());
+        grid.add(immigrantEmail, 1, 9);
 
 
         //--------------------------------------------------------------------------------//
@@ -126,30 +144,30 @@ public class ReviewUI extends Application {
 
 
         Label Spacer02 = new Label("");
-        grid.add(Spacer02, 0, 9, 2, 2);
+        grid.add(Spacer02, 0, 10, 2, 2);
 
         Label petitionTitle = new Label("------------ Petition Information ------------");
-        grid.add(petitionTitle, 0, 11, 2, 2);
+        grid.add(petitionTitle, 0, 12, 2, 2);
 
         Label petitionIDTitle = new Label("PetitionID:");
-        grid.add(petitionIDTitle, 0, 13);
-        TextField petitionIDAns = new TextField(petitionID);
-        grid.add(petitionIDAns, 1, 13);
+        grid.add(petitionIDTitle, 0, 14);
+        TextField petitionIDAns = new TextField(String.valueOf(petition.getPetitionID()));
+        grid.add(petitionIDAns, 1, 14);
 
         Label petitionerIDTitle = new Label("PetitionerID:");
-        grid.add(petitionerIDTitle, 0, 14);
-        TextField petitionerIDAns = new TextField(petitionerID);
-        grid.add(petitionerIDAns, 1, 14);
+        grid.add(petitionerIDTitle, 0, 15);
+        TextField petitionerIDAns = new TextField(String.valueOf(petition.getPetitionerID()));
+        grid.add(petitionerIDAns, 1, 15);
 
         Label submissionDateTitle = new Label("Submission Date:");
-        grid.add(submissionDateTitle, 0, 15);
-        TextField submissionDateAns = new TextField(submissionDate);
-        grid.add(submissionDateAns, 1, 15);
+        grid.add(submissionDateTitle, 0, 16);
+        TextField submissionDateAns = new TextField(petition.getSubmissionDate());
+        grid.add(submissionDateAns, 1, 16);
 
         Label statusTitle = new Label("Status:");
-        grid.add(statusTitle, 0, 16);
-        TextField statusAns = new TextField(status);
-        grid.add(statusAns, 1, 16);
+        grid.add(statusTitle, 0, 17);
+        TextField statusAns = new TextField(petition.getStatus());
+        grid.add(statusAns, 1, 17);
 
 
         //--------------------------------------------------------------------------------//
@@ -158,8 +176,13 @@ public class ReviewUI extends Application {
 
 
         Label Spacer03 = new Label("");
-        grid.add(Spacer03, 0, 17, 2, 2);
+        grid.add(Spacer03, 0, 18, 2, 2);
 
+        // SPACING
+        Label modCountLab = new Label("");
+        grid.add(modCountLab, 0, 21);
+
+        // DENIAL BUTTON
         Button denialButton = new Button("<");
         denialButton.setOnAction(e -> {
             try {
@@ -168,12 +191,29 @@ public class ReviewUI extends Application {
                 ex.printStackTrace();
             }
         });
-        grid.add(denialButton, 0, 18);
+        grid.add(denialButton, 0, 19);
 
+        // SAVE BUTTON
         Button saveButton = new Button("save");
-        grid.add(saveButton, 1, 18);
+        saveButton.setOnAction(e -> {
+            immigrant.setName(immigrantFirstName.getText(), immigrantLastName.getText());
+            immigrant.setEmail(immigrantEmail.getText());
+            immigrant.setAddress(immigrantAddress.getText());
+            immigrant.setBirthdate(immigrantBD.getText());
 
-        Button approvalButton = new Button("Approval");
+            petition.setPetitionID(Integer.parseInt(petitionIDAns.getText()));
+            petition.setPetitionerID(Integer.parseInt(petitionerIDAns.getText()));
+            petition.setStatus(statusAns.getText());
+            petition.setSubmissionDate(submissionDateAns.getText());
+
+            modCount.getAndIncrement();
+            modCountLab.setText("save count: " + modCount);
+
+        });
+        grid.add(saveButton, 1, 19);
+
+        // APPROVAL BUTTON
+        Button approvalButton = new Button(">");
         approvalButton.setOnAction(e -> {
             try {
                 new ApprovalUI().start(primaryStage);
@@ -181,8 +221,33 @@ public class ReviewUI extends Application {
                 ex.printStackTrace();
             }
         });
-        grid.add(approvalButton, 2, 18);
+        grid.add(approvalButton, 2, 19);
 
+
+        // REVERT BUTTON
+        Button revertButton = new Button("revert");
+        revertButton.setOnAction(e -> {
+            immigrantFirstName.setText(immigrant.getFirst_name());
+            immigrantLastName.setText(immigrant.getLast_name());
+            immigrantEmail.setText(immigrant.getEmail());
+            immigrantAddress.setText(immigrant.getAddress());
+            immigrantBD.setText(immigrant.getBirthdate());
+
+            petitionIDAns.setText(String.valueOf(petition.getPetitionID()));
+            petitionerIDAns.setText(String.valueOf(petition.getPetitionerID()));
+            statusAns.setText(String.valueOf(petition.getStatus()));
+            submissionDateAns.setText(String.valueOf(petition.getSubmissionDate()));
+        });
+        grid.add(revertButton, 0, 20);
+
+
+
+        // FETCH BUTTON
+        Button fetchButton = new Button("another");
+        fetchButton.setOnAction(e -> {
+
+        });
+        grid.add(fetchButton, 2, 20);
 
         Scene scene = new Scene(grid, 500, 700);
         primaryStage.setScene(scene);
